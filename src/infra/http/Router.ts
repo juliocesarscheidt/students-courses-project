823b0065dfd54edd359fe6304405152ae19d3d28
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-02-12 14:04:36 -0300

    Add docker-compose and health routes

--- src/infra/http/Router.ts
+++ src/infra/http/Router.ts
@@ -9,8 +9,10 @@ import GetCourseUsecase from "../../application/usecase/course/GetCourseUsecase"
 import GetStudentUsecase from "../../application/usecase/student/GetStudentUsecase";
 import ListCourseByAreaUsecase from "../../application/usecase/course/ListCourseByAreaUsecase";
 import ListCourseByStudentUsecase from "../../application/usecase/course/ListCourseByStudentUsecase";
+import HealthCheckController from "../controllers/HealthCheckController";
 
 export default class Router {
+  healthCheckController: HealthCheckController;
   courseController: CourseController;
   studentController: StudentController;
 
@@ -32,6 +34,7 @@ export default class Router {
     const getStudentUsecase = new GetStudentUsecase(studentRepository);
 
     // controllers
+    this.healthCheckController = new HealthCheckController();
     this.courseController = new CourseController(createCourseUsecase, getCourseUsecase,
       listCourseByAreaUsecase, listCourseByStudentUsecase, subscribeStudentToCourseUsecase);
     this.studentController = new StudentController(createStudentUsecase, getStudentUsecase);
@@ -40,26 +43,27 @@ export default class Router {
   }
 
   setupRoutes() {
+    // health check
+    this.http.on("/v1/health/live", "get", async (params: any, body: any, query: any, headers: any) =>
+      this.healthCheckController.getLiveness(params));
+    this.http.on("/v1/health/ready", "get", async (params: any, body: any, query: any, headers: any) =>
+      this.healthCheckController.getReadiness(params));
+
     // course
     this.http.on("/v1/course", "post", async (params: any, body: any, query: any, headers: any) =>
       this.courseController.createCourse(params, body));
-
     this.http.on("/v1/course/:coursePk", "get", async (params: any, body: any, query: any, headers: any) =>
       this.courseController.getCourse(params, body));
-
     this.http.on("/v1/course/area/:area", "get", async (params: any, body: any, query: any, headers: any) =>
       this.courseController.listCourseByArea(params, body));
-
     this.http.on("/v1/course/student/:studentPk", "get", async (params: any, body: any, query: any, headers: any) =>
       this.courseController.listCourseByStudent(params, body));
-
     this.http.on("/v1/course/:coursePk/subscribe/:studentPk", "put", async (params: any, body: any, query: any, headers: any) =>
       this.courseController.subscribeStudentToCourse(params, body));
 
     // student
     this.http.on("/v1/student", "post", async (params: any, body: any, query: any, headers: any) =>
       this.studentController.createStudent(params, body));
-
     this.http.on("/v1/student/:studentPk", "get", async (params: any, body: any, query: any, headers: any) =>
       this.studentController.getStudent(params, body));
   }
